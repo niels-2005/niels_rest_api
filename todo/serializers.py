b12ded3313d77fd547bb6ed7f23802d16ac33ac7
niels-2005 <niels.scholz01@web.de>
2023-06-10 22:47:47 +0200

    added user to sequliazer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,19 @@
 from .models import Todo
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "username", "email"]
 
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Todo
-        fields = ["id", "title", "description", "created_at"]
+        fields = ["id", "title", "description", "created_at", "user"]
--- up.bat
+++ up.bat
@@ -1,3 +1,4 @@
 git add .   
 git commit -m "%*"
 git push
+chrome "https://www.pythonanywhere.com/user/nielsscholz/consoles/29053074/" 
