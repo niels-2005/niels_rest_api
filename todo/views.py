b77ffd44c5c12021d752a4d18bc16c5fbf271524
niels-2005 <niels.scholz01@web.de>
2023-06-10 22:03:19 +0200

    first working version

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer
+
 
 # Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = Todo.objects.all().order_by("-created_at")
+    serializer_class = TodoSerializer
+    permission_classes = []  # permissions.IsAuthenticated
