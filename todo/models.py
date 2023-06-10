16ab32aa98beb77c264a24ebbb0d4468e8d47b49
niels-2005 <niels.scholz01@web.de>
2023-06-10 14:14:28 +0200

    break

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import datetime
+from django.conf import settings
+
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a55ax(hdu(n-d=hcx#fnebwd(hj#i9fvpn@a+jkny@i60)@^h*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["nielsscholz.pythonanywhere.com", "localhost"]
+ALLOWED_HOSTS = ["nielsscholz.pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo",
 ]
 
 MIDDLEWARE = [
